7cebf30e5600ba2244296cb17d840833af866461
iamsahana10 <sahanabangadi76@gmail.com>
2026-02-09 00:35:29 -0800

    services created for auth

---
+++ src/services/auth.service.js
@@ -0,0 +1,23 @@
+export const registerUser=async(data)=>{
+    try{
+        const res=await api.post("/auth/register",data);
+        console.log("user.data:",res.data);
+        return res.data;
+    }catch(err){
+        console.error(err);
+    }
+}
+export const loginUser=async(data)=>{
+    try{
+        const res=await api.post("/auth/login",data);
+        localStorage.setItem("token",res.data.token);
+        return res.data;
+    }catch(err){
+        console.error(err);
+    }
+}
+export const logoutUser=()=>{
+    localStorage.removeItem("token");
+    window.location.href="/login";
+}
+export const isAuthenticated=()=>!!localStorage.getItem("token");
