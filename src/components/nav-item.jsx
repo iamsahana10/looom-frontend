dbafbc63a6d36b24ddcc993103532cf4dffb5441
iamsahana10 <sahanabangadi76@gmail.com>
2026-03-23 02:04:33 -0700

    Update nav-item.jsx

--- src/components/nav-item.jsx
+++ src/components/nav-item.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NAV_ICON_ACTIVE = "text-gray-800 group-hover:text-black ";
 const NAV_ICON_INACTIVE = "text-gray-400 group-hover:text-black ";
-const NavItem = ({ active, size = 22 }) => {
+const NavItem = ({ Icon, active, size = 22 }) => {
   return (
     <Icon
       size={size}
